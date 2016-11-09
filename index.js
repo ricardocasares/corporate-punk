370a0b372546a09281da4004cc96667dbf98d2a4
Ricardo Casares <ricardocasares@gmail.com>
2016-11-09 10:53:40 +0100

    change structure

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const exp = require('express')
 const mw = require('./middleware')
 const log = require('debug')('punk')
 const app = exp()
+const index = require('serve-index')
+const http = require('http')
+const config = require('./config')
+const argv = require('minimist')(process.argv.slice(2))
+const normalize = require('./lib').normalizePort
+const port = normalize(argv.port || config.port)
+
+config.cache = argv.cache || config.cache
+config.images = argv.images || config.images
 
 app
+    .set('port', port)
     .use('/', exp.static(__dirname + '/ui'))
+    .use('/list', index(config.images, {icons: true}))
     .get('/hold',
         mw.randomImage,
         mw.specificImage,
@@ -18,4 +29,6 @@ app
     .get('/cache/clear', mw.clearCache)
     .use(mw.errorHandler)
 
-module.exports = app
+const server = http.createServer(app)
+
+module.exports = server
